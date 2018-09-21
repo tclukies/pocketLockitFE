3a6cec1ab13d796a2c680e9a7895b4a32119f5f7
Thomas Clukies <Thomas@Thomass-MacBook-Pro.local>
2018-09-21 13:01:22 -0600

    trying to test socket again

--- frontEnd/lock.js
+++ frontEnd/lock.js
@@ -1,15 +1,15 @@
-// var socket = io(); //load socket.io-client and connect to the host that serves the page
+var socket = io(); //load socket.io-client and connect to the host that serves the page
 
-// window.addEventListener("load", function(){ //when page loads
-//     var lightbox = document.getElementById("light"); 
-//     lightbox.addEventListener("change", function() { //add event listener for when checkbox changes
-//       socket.emit("light", Number(this.checked)); //send button status to server (as 1 or 0)
-//     });
-//   });
-//   socket.on('light', function (data) { //get button status from client
-//     document.getElementById("light").checked = data; //change checkbox according to push button on Raspberry Pi
-//     socket.emit("light", data); //send push button status to back to server
-//   });
+window.addEventListener("load", function(){ //when page loads
+    var lightbox = document.getElementById("light"); 
+    lightbox.addEventListener("change", function() { //add event listener for when checkbox changes
+      socket.emit("light", Number(this.checked)); //send button status to server (as 1 or 0)
+    });
+  });
+  socket.on('light', function (data) { //get button status from client
+    document.getElementById("light").checked = data; //change checkbox according to push button on Raspberry Pi
+    socket.emit("light", data); //send push button status to back to server
+  });
 
 document.querySelector("#open-lock").addEventListener("click", unlock);
 
